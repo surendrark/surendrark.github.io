5436a1d43a92fff0d538c6d28f548635309f986b
Surendra Reddy Kancharla <surendrareddy.kancharla@gmail.com>
2019-09-02 20:40:59 +0530

    Updated

---
+++ assets/ntheme/js/custom.js
@@ -0,0 +1,11 @@
+/*
+------------------------------------------------------------------------
+* Template Name    : Elvish | Responsive Bootstrap 4 Personal Template * 
+* Author           : ThemesBoss                                        *
+* Version          : 1.0.0                                             *
+* Created          : May 2018                                          *
+* File Description : Main Js file of the template                      *
+*-----------------------------------------------------------------------
+*/
+
+        $("#status").fadeOut(),$("#preloader").delay(350).fadeOut("slow"),$("body").delay(350).css({overflow:"visible"}),$(window).on("scroll",function(){$(window).scrollTop()>=800?$(".sticky").addClass("stickyadd"):$(".sticky").removeClass("stickyadd")});
